6dafe499172074ffcc2cc241d42cf8cfeccc7244
Stéphane Depierrepont <toorop@gmail.com>
2022-02-16 14:31:52 +0100

    preload option

--- main.js
+++ main.js
@@ -1,36 +1,24 @@
 import './style.css'
-import toBuffer  from 'it-to-buffer'
+import toBuffer from 'it-to-buffer'
 
-const cid='QmbmesdSusHcvhZwaE4KS8AhhQZS8KEidbiouns3zDCtPX'
+const cid = 'QmbmesdSusHcvhZwaE4KS8AhhQZS8KEidbiouns3zDCtPX'
 
-const main = async () => {
-  const node = await IPFS.create()
-  console.log(node)
-  const buffer = await toBuffer(node.cat(cid))
-  const blob = new Blob([buffer])
-  const urlCreator = window.URL || window.webkitURL
-  const imageUrl = urlCreator.createObjectURL(blob)
-  const img = document.getElementById('my-image')
-  img.src = imageUrl
-
-  updatePeersCount(node)
-
-}
-
-const updatePeersCount = (node) => {
+/*const updatePeersCount = (node) => {
   const peers = document.getElementById('peers')
   setInterval(async () => {
     console.log(await node.swarm.peers())
     //peers.innerHTML = await  node.swarm.peers()
   }, 1000)
-}
+}*/
 
 const init = async () => {
   console.log('Init IPFS')
   const IpfsModule = await import('./src/modules/ipfs-core/ipfs-core.js')
   const IPFS = IpfsModule.default;
   console.log(IPFS)
-  const node = await IPFS.create()
+  const node = await IPFS.create({
+    preload: {enabled: true},
+  })
   console.log(node)
   const identity = await node.id()
   const nodeId = identity.id
