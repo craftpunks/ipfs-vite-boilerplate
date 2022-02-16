3c1e04c91534c72ab80fe8bf02d3dc84aef4562f
Stéphane Depierrepont <toorop@gmail.com>
2022-02-16 12:00:30 +0100

    poc

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
+        main: resolve(__dirname, 'index.html'),
       }
     }
   }
